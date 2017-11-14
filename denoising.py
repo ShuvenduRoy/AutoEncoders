a7c9891f6b93ccf9b4c8ebc6d4bdd524acd8c7ae
Shuvendu Bikash <bikash11roy@gmail.com>
2017-11-14 18:12:38 +0600

    indexing bug fixed
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- denoising.py
+++ denoising.py
@@ -60,14 +60,14 @@ n = 10
 plt.figure(figsize=(20, 4))
 for i in range(n):
     # display original
-    ax = plt.subplot(2, n, i+1)
+    ax = plt.subplot(2, n, i)
     plt.imshow(x_test[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
     # display reconstruction
-    ax = plt.subplot(2, n, i + 1+ n)
+    ax = plt.subplot(2, n, i + n)
     plt.imshow(decoded_imgs[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
