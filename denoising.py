89bd4b02d8d1ed7865051c61c09105ae6aa0348b
Shuvendu Bikash <bikash11roy@gmail.com>
2017-11-14 18:26:51 +0600

    denoising
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- denoising.py
+++ denoising.py
@@ -1,5 +1,6 @@
 from keras.datasets import mnist
 import numpy as np
+import matplotlib.pyplot as plt
 
 # loading the dataset
 (x_train, _), (x_test, _) = mnist.load_data()
@@ -8,8 +9,23 @@ x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
 x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
-print (x_train.shape)
-print (x_test.shape)
+
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
+
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
+n = 10
+plt.figure(figsize=(20, 2))
+for i in range(n):
+    ax = plt.subplot(1, n, i+1)
+    plt.imshow(x_test_noisy[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
 
 
 """Building the model"""
@@ -44,7 +60,7 @@ from keras.callbacks import TensorBoard
 
 
 # training the model
-autoencoder.fit(x_train, x_train,
+autoencoder.fit(x_train_noisy, x_train,
                 epochs=5,
                 batch_size=256,
                 shuffle=True,
@@ -53,15 +69,14 @@ autoencoder.fit(x_train, x_train,
 
 
 # Visualizing
-import matplotlib.pyplot as plt
-decoded_imgs = autoencoder.predict(x_test)
+decoded_imgs = autoencoder.predict(x_test_noisy)
 
 n = 10
 plt.figure(figsize=(20, 4))
 for i in range(n):
     # display original
     ax = plt.subplot(2, n, i)
-    plt.imshow(x_test[i].reshape(28, 28))
+    plt.imshow(x_test_noisy[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
